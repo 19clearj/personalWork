bf558f21c4d7e15545e8e6b21ce75b648836d5df
Joey <19clearj@elmbrookstudents.org>
2018-09-13 13:53:40 -0500

    Name and mailing label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Joey Cleary"
+				+ "\n13500 Richard Rd"
+				+ "\nBrookfield, Wisconsin 53005 ");
+	}
+
+}
