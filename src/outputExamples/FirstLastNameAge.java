3dd122cafc35135d25d4df8f1730e7b2a43dc4bf
Joey <19clearj@elmbrookstudents.org>
2018-09-13 13:17:19 -0500

    First Last Name and Age Program

---
+++ src/outputExamples/FirstLastNameAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstLastNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Joey");
+		System.out.println("Cleary");
+		System.out.println(18);
+	}
+
+}
