07bc298a22a12329c1ec4494255f44b56d02979b
Joey <19clearj@elmbrookstudents.org>
2018-10-01 14:42:26 -0500

    Finished contrsuctors and used them in class

---
+++ src/oop/testClass.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter bob = new Greeter();
+		bob.sayHello();
+		Friend joe = new Friend();
+		joe.sayMessage();
+		
+	}
+
+}
