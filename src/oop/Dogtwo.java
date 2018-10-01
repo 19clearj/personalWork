8fe570f70eeb99df85ccbfc3308a1b7b25b6523a
Joey <19clearj@elmbrookstudents.org>
2018-10-01 15:01:08 -0500

    OOP examples plus dog constructor and test class

---
+++ src/oop/Dogtwo.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Dogtwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog dog = new Dog();
+		dog.sayMessage();Greeter bob = new Greeter();
+		dog.attack();
+	}
+
+}
