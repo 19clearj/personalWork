8eb12360aeac4d0c35800b7d1c0b811a1e81ee02
Joey <19clearj@elmbrookstudents.org>
2018-10-03 14:46:31 -0500

    Area Constructor with and without door

---
+++ src/oop/AreaTestClass.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class AreaTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AreaConstructor area = new AreaConstructor ();
+		area.area();
+		area.areaWithDoor();
+		
+	}
+
+}
