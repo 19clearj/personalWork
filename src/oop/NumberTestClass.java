68a6d64b3e59e0c4c58028bf59fe49b947face62
Joey <19clearj@elmbrookstudents.org>
2018-10-04 13:45:32 -0500

    number parameter

---
+++ src/oop/NumberTestClass.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class NumberTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Numbers bob = new Numbers();
+bob.sayNumber(7.5);
+	}
+
+}
