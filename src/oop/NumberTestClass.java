ecc42aab9963a8ac63bf5986283bcc1b7e3ef416
Joey <19clearj@elmbrookstudents.org>
2018-10-05 15:05:39 -0500

    return types in numbers

--- src/oop/NumberTestClass.java
+++ src/oop/NumberTestClass.java
@@ -6,6 +6,14 @@ public class NumberTestClass {
 		// TODO Auto-generated method stub
 Numbers bob = new Numbers();
 bob.sayNumber(7.5);
+bob.sayNumberPlus2(7.5);
+bob.saySum(1.5, 2.5);
+double num = bob.returnSquare(4.0);
+System.out.println("Your number is " + num);
+double area = bob.returnArea(4.0, 5.0);
+System.out.println("The area is " + area);
+int round = bob.returnRoundUp(4.3);
+System.out.println("Your number rounded is " + round);
 	}
 
 }
