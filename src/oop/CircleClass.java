660c15526cea258734ad6a79f5e006b652ccc300
Joey <19clearj@elmbrookstudents.org>
2018-10-10 15:04:39 -0500

    Circle and Rectangle get and set

---
+++ src/oop/CircleClass.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class CircleClass {
+	//instance fields
+private double radius;
+//constructors
+public CircleClass(){
+	radius = 0.0;
+}
+public CircleClass(double newRadius){
+	radius = newRadius;
+}
+//mutator methods
+public void setRadius(double newRadius){
+	radius = newRadius;
+}
+//accessor methods
+public double getRadius(){
+	return radius;
+}
+public double circumference(){
+
+	return 2*Math.PI*radius;
+}
+public double area(){
+
+	return Math.PI*radius*radius;
+}
+}
