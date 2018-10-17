3c4d07b5814a0be0ce3025d9edb6de3026da15f5
Joey <19clearj@elmbrookstudents.org>
2018-10-17 14:32:16 -0500

    edits pet garage

--- src/decisions/PetsGarage.java
+++ src/decisions/PetsGarage.java
@@ -12,7 +12,7 @@ public class PetsGarage {
 		double pets = input.nextDouble();
 		System.out.println("How many cars can fit in your garage");
 		double cars = input.nextDouble();
-		System.out.println("you have at least 5 pets and can fit more than 3 cars is your garage" + (pets>=5 || cars > 3));
+		System.out.println("you have at least 5 pets and can fit more than 3 cars is your garage . . ." + (pets>=5 || cars > 3));
 	}
 
 }
