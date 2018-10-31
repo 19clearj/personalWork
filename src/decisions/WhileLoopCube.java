87cc9401543e189643404a1b5c25c327cba5f48b
Joey <19clearj@elmbrookstudents.org>
2018-10-31 14:57:05 -0500

    Loop Fun

--- src/decisions/WhileLoopCube.java
+++ src/decisions/WhileLoopCube.java
@@ -5,7 +5,7 @@ public class WhileLoopCube {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter an integer: (enter -1 to quit)");
+		System.out.println("Enter an integer: ");
 		int number = input.nextInt();
 	while	(number != -1){
 		System.out.println(number*number*number);
